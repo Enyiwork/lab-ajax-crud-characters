da29aa457690d9964c0305a7fba48f47436c190a
Enyiwork <32691424+Enyiwork@users.noreply.github.com>
2017-11-30 08:28:04 -0500

    first and second part finish

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,11 +4,68 @@ class APIHandler {
   }
 
   getFullList () {
+     $(".character-info").html(`<h2> Loading ... </h2>`);
 
+     $.ajax({
+        method: "GET",
+        url: `${this.BASE_URL}/characters/`
+     })
+     .then((apiResult) => {
+       console.log("SUCCESS");
+       console.log(apiResult);
+
+       $(".characters-container").empty();
+
+       apiResult.forEach((onechar) => {
+         $(".characters-container").append(`
+
+         <div class="character-info">
+
+           <div class="name">       ${onechar.name}       </div>
+           <div class="occupation"> ${onechar.occupation} </div>
+           <div class="debt">       ${onechar.debt}       </div>
+           <div class="weapon">     ${onechar.weapon}     </div>
+           <div class="id">ID:      ${onechar.id}         </div>
+         </div>
+       `);
+      });
+     })
+     .catch((err) => {
+       console.log("ERROR");
+       console.log(err);
+     });
   }
 
-  getOneRegister () {
+  getOneRegister (idNumber) {
 
+    $.ajax({
+      method: "GET",
+      url: `${this.BASE_URL}/characters/${idNumber}`
+    })
+    .then((apiResult) => {
+      console.log("SUCCESS");
+      console.log(apiResult);
+
+      $(".characters-container").empty();
+
+      apiResult.forEach((oneChar) => {
+         $(".characters-container").append(`
+           <div class="character-info">
+
+             <div class="name">       ${onechar.name}       </div>
+             <div class="occupation"> ${onechar.occupation} </div>
+             <div class="debt">       ${onechar.debt}       </div>
+             <div class="weapon">     ${onechar.weapon}     </div>
+
+           </div>
+           `);
+        });
+     })
+    .catch((err) => {
+      console.log("ERROR");
+      console.log(err);
+
+    });
   }
 
   createOneRegister () {
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,27 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+     console.log("Fetch-all");
+     charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+     console.log("Fetch-one");
+      event.preventDefault();
+      const idNumber = $("#character-id").val();
+      charactersAPI.getOneRegister(idNumber);
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+     console.log("delete-one");
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+     console.log("edit-char");
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+     console.log("new-char");
+  });
+});
